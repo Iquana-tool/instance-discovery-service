73ea98acf9d0863ae3b76fcf8182da485ae2a8bf
Robert Leist <robert.leist@dfki.de>
2025-11-25 14:46:20 +0100

    updated

--- app/routes/models.py
+++ app/routes/models.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, HTTPException
-from app.state import MODEL_REGISTRY, MODEL_CACHE
 from logging import getLogger
 
+from fastapi import HTTPException
 
-api_router = APIRouter(prefix="/models", tags=["models"])
+from app.routes import router
+from app.state import MODEL_REGISTRY, MODEL_CACHE
+
 logger = getLogger(__name__)
 
 
-@api_router.get("/available")
+@router.get("/available")
 async def list_models():
     """ Lists all available models in the registry. """
     available_models = MODEL_REGISTRY.list_models(only_return_available=True)
@@ -17,7 +18,7 @@ async def list_models():
         "available_models": available_models}
 
 
-@api_router.get("/load_model/model_id={model_id}&user_id={user_id}")
+@router.get("/load_model/model_id={model_id}&user_id={user_id}")
 async def load_model(model_id: int, user_id: int):
     """ Loads a model into the cache if not already loaded. This is a convenience endpoint; models are loaded
         automatically when needed, but this can be called at the start
--- app/routes/upload_image.py
+++ app/routes/upload_image.py
@@ -1,11 +1,11 @@
 from logging import getLogger
-from fastapi import APIRouter, UploadFile, File
 
+from fastapi import UploadFile, File
+
+from app.routes import router
 from app.state import IMAGE_CACHE
 from util.image_loading import load_image_from_upload
 
-
-router = APIRouter(prefix="/annotation_session", tags=["annotation_session"])
 logger = getLogger(__name__)
 
 
